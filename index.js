591ac8ca5230397dbad123f0bb567545f9f4b643
Fernando <fer3443@gmail.com>
2023-08-11 11:12:42 -0300

    archivo de logica iniciado

---
+++ index.js
@@ -0,0 +1,12 @@
+const btnSubmit = document.querySelector("#btnSubmit");
+
+const inputTitle = document.querySelector("#taskTitle");
+const inputDescription = document.querySelector("#taskDescription");
+const inputDate = document.querySelector('#taskDate');
+const inputTime = document.querySelector('#taskTime');
+
+
+btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault()
+    
+})
